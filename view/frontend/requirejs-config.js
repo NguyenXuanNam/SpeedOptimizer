c5ba42379813fc198489c3603b49ab09531bb24b
Nam <nam.nx@gnt.com.vn>
2022-10-29 01:25:12 +0700

    init source

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,13 @@
+var config = {
+
+	paths: {
+		'namnguyen/lazyload'			: 'NamNguyen_SpeedOptimizer/js/plugins/lazyload.min',
+	},
+
+	shim: {
+		'namnguyen/lazyload': {
+			deps: ['jquery']
+		}
+	}
+
+};
